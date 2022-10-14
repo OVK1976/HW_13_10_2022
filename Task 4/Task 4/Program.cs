abcda10921a3efed063d2c9d6def9e36c8acfe32
OVK1976 <olegkalinin@yandex.ru>
2022-10-14 10:06:56 +0400

    Решение задачи 4
    Сделали решение задачи № 4

---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Пример
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 
+Уточнения: числа целые*/
+Console.Write("Введите число 1: ");
+string a = Console.ReadLine();
+Console.Write("Введите число 2: ");
+string b = Console.ReadLine();
+Console.Write("Введите число 3: ");
+string c = Console.ReadLine();
+Console.WriteLine($"Результаты выбора максимального из чисел: {a} {b} {c} ");
+int a1 = int.Parse(a);
+int b1 = int.Parse(b);
+int c1 = int.Parse(c);
+int max = a1;
+if (a1 > max) max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+Console.WriteLine($"max={max}");
+Console.WriteLine("Расчеты завершены");
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Пример:
+a = 5; b = 7->max = 7
+a = 2 b = 10->max = 10
+ a = -9 b = -3->max = -3 
+Уточнения: числа целые, проверка равенства значений не предусмотрена */
+Console.Write("Введите число 1: ");
+string a=Console.ReadLine();
+Console.Write("Введите число 2: ");
+string b = Console.ReadLine();
+int a1=int.Parse(a);
+int b1=int.Parse(b);
+if (a1>b1)
+{
+    Console.WriteLine($"max={a1} min={b1}");
+}
+else
+{
+    Console.WriteLine($"max={b1} min={a1}");
+}
+Console.WriteLine("Расчеты завершены");
